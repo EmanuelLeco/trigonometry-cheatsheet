b2ae1979af0cc31cd5a913c6139028674aa1f509
EmanuelLeco <leco.emanuel@gmail.com>
2021-08-31 13:09:45 +0200

    Cosine line

--- src/components/UnitCircle.vue
+++ src/components/UnitCircle.vue
@@ -8,16 +8,20 @@
 		xmlns="http://www.w3.org/2000/svg"
 	>
 		<circle cx="250" cy="250" r="100" />
+
+		<!-- __ COS __ -->
 		<line x1="0" y1="250" x2="500" y2="250" />
+		<line id="cos" x1="250" y1="250" :x2="cosLength" y2="250" />
+		<!-- --------- -->
 
 		<!-- __ SIN __ -->
 		<line x1="250" y1="0" x2="250" y2="500" />
 		<line id="sin" x1="250" y1="250" x2="250" :y2="sinLength" />
-		<!-- __ __ -->
+		<!-- --------- -->
 
 		<!-- -- angle line -- -->
 		<line x1="250" y1="250" :x2="endOfLine.x" :y2="endOfLine.y" />
-		<!-- -- -- -->
+		<!-- ---------------- -->
 		<!-- position relative to svg fix -->
 		<rect width="500" height="500" />
 	</svg>
@@ -65,6 +69,9 @@
 				return baseAng < 0 ? baseAng + 2 * Math.PI : baseAng;
 			},
 
+			cosLength() {
+				return 100 * Math.cos(this.angle) + 250;
+			},
 			sinLength() {
 				return -100 * Math.sin(this.angle) + 250;
 			},
