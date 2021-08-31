7d4b4f20e22d9208013d54b269b61ea95b308f04
EmanuelLeco <leco.emanuel@gmail.com>
2021-08-31 12:55:33 +0200

    add line moving to mouse pointer

--- src/components/UnitCircle.vue
+++ src/components/UnitCircle.vue
@@ -8,14 +8,63 @@
 		xmlns="http://www.w3.org/2000/svg"
 	>
 		<circle cx="250" cy="250" r="100" />
-		<line class="dashed" x1="0" y1="250" x2="500" y2="250" />
-		<line class="dashed" x1="250" y1="0" x2="250" y2="500" />
+		<line x1="0" y1="250" x2="500" y2="250" />
+		<line x1="250" y1="0" x2="250" y2="500" />
+		<!-- -- angle line -- -->
+		<line x1="250" y1="250" :x2="endOfLine.x" :y2="endOfLine.y" />
+		<!-- -- -- -->
+		<!-- position relative to svg fix -->
+		<rect width="500" height="500" />
 	</svg>
 </template>
 
 <script>
 	export default {
 		name: 'UnitCircle',
+		data() {
+			return {
+				mousePosition: {
+					x: 0,
+					y: 0,
+				},
+			};
+		},
+		computed: {
+			// mouse position relative to the center of the graph
+			a() {
+				return this.mousePosition.x - 250;
+			},
+			b() {
+				return this.mousePosition.y - 250;
+			},
+
+			endOfLine() {
+				let x, y;
+				let a = this.a,
+					b = this.b;
+
+				x = a < 0 ? 0 : 500;
+				y = b < 0 ? 0 : 500;
+
+				if (Math.abs(a) > Math.abs(b)) {
+					y = Math.round((b * 250) / Math.abs(a)) + 250;
+				} else if (Math.abs(a) < Math.abs(b)) {
+					x = Math.round((a * 250) / Math.abs(b)) + 250;
+				}
+
+				return { x: x, y: y };
+			},
+		},
+		methods: {
+			getPosition(e) {
+				// update mouse position props
+				let rect = e.target.getBoundingClientRect();
+				this.mousePosition = {
+					x: e.clientX - rect.left,
+					y: e.clientY - rect.top,
+				};
+			},
+		},
 	};
 </script>
 
@@ -31,5 +80,10 @@
 	line {
 		stroke: var(--fg);
 		stroke-width: 5px;
+		stroke-linecap: round;
+	}
+
+	rect {
+		opacity: 0;
 	}
 </style>
