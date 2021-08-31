e2c06bbe3748d3a8d89fb8ef2ae1b33a30e2770a
EmanuelLeco <leco.emanuel@gmail.com>
2021-08-31 11:53:13 +0200

    create color theme

--- src/App.vue
+++ src/App.vue
@@ -8,3 +8,15 @@
 		components: {},
 	};
 </script>
+
+<style>
+	:root {
+		--sin: #3cc386;
+		--cos: #433cc3;
+		--tg: #c33c79;
+		--cotg: #bcc33c;
+
+		--fg: #324d7e;
+		--bg: #1a263d;
+	}
+</style>
